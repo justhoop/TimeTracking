28d3f7e612987077e9e8438c98008cae4260b40a
Justin <20794376+justhoop@users.noreply.github.com>
2022-02-14 08:34:06 -0500

    report on hours worked

---
+++ Report.ps1
@@ -0,0 +1,36 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $StartDate,
+    [string]
+    $EndDate
+)
+
+function Get-SortableDate {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [DateTime]
+        $date
+    )
+    $now = get-date -Date $date
+    $month = $now.Month.ToString()
+    if ($month.Length -eq 1) { $month = "0" + $month }  
+    $day = $now.Day.ToString()
+    if ($day.Length -eq 1) { $day = "0" + $day }  
+    $result = $now.Year.ToString() + "-" + $month + "-" + $day
+    return $result
+}
+
+$files = Get-ChildItem "*.csv"
+foreach($file in $files){
+    if (($file.name.split('.')[0] -ge (Get-SortableDate $StartDate)) -and ($file.name.split('.')[0] -le (Get-SortableDate $EndDate))) {
+        $day = Import-Csv $file
+        $hours = 0
+        foreach($entry in $day){
+            $hours += $entry.Total
+        }
+        Write-Host $file.name.split('.')[0]" - "$hours
+    }
+}
